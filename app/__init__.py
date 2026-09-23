f3f57ab737f020d09a76d97323e7a894e697103d
Sukran K <ksukran2005@gmail.com>
2026-09-23 21:56:39 +0530

    Configure environment variables and dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
     @app.route("/")
     def home():
